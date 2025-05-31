f735cf2031c2668914ff7b7a5a9c558f9fabcb0d
Shrey719 <shrey150436@gmail.com>
2025-05-30 21:02:12 -0400

    made this thing from 23432 seperate sources but it now has a better search function

--- lib/browser_search.lua
+++ lib/browser_search.lua
@@ -1,1 +1,35 @@
-os.execute('xdg-open https://duckduckgo.com/?q='..table.concat(arg, "%20", 1))
+local query = table.concat(arg, " ", 1)
+local query_s = table.concat(arg, "%20", 1)
+
+--ilovestackoverflowilovestackoverflow
+function qran(str)
+    str = str:match("^%s*(.-)%s*$")
+
+    if str == "" then
+      return "empty"
+    end
+    if str:match("^[a-z]+://[%w%.%-_%%]+[%w%p]*$") then
+      return "url"
+    end
+  
+    if str:match("^localhost:?%d*/?.*") or str:match("^127%.0%.0%.1:?%d*/?.*") then
+      return "url"
+    end
+  
+    if str:match("^%w[%w%-%.]*%.%a%a+/?[%w%p]*$") then
+      return "url"
+    end
+  
+    if str:find(" ") or str:find("[?&=]") then
+      return "query"
+    end
+  
+    return "unknown"  
+end
+print(qran(query))
+if qran(query) == "url" then
+    os.execute("xdg-open "..query)
+    -- if it dont match, just search it
+else os.execute('xdg-open https://searx.be/search?q='..query_s)
+end
+
