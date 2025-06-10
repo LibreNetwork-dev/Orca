2bec82db8207c73c177ce5f10873eaa0fbbce856
Shrey719 <shrey150436@gmail.com>
2025-06-10 18:00:05 -0400

    fix searching

--- lib/search.lua
+++ lib/search.lua
@@ -1,6 +1,31 @@
 local query = table.concat(arg, " ", 1)
 local query_s = table.concat(arg, "%20", 1)
 
+function openUrl(url)
+  local is_windows = package.config:sub(1,1) == '\\'
+  local function commandExists(cmd)
+    local handle = io.popen("command -v " .. cmd .. " 2>/dev/null")
+    local result = handle:read("*a")
+    handle:close()
+    return result ~= ''
+  end
+
+  if is_windows then
+    -- Windows (because maybe just in case)
+    os.execute('start "" "' .. url .. '"')
+  else
+    -- linux (not universial but idgas atp)
+    if commandExists("xdg-open") then
+      os.execute('xdg-open "' .. url .. '" &')
+    elseif commandExists("open") then
+      -- macOS  (because maybe just in case)
+      os.execute('open "' .. url .. '" &')
+    else
+      print("Could not open url.")
+    end
+  end
+end
+
 --ilovestackoverflowilovestackoverflow
 function qran(str)
     str = str:match("^%s*(.-)%s*$")
@@ -28,8 +53,8 @@ function qran(str)
 end
 print(qran(query))
 if qran(query) == "url" then
-    os.execute("xdg-open "..query)
+  openUrl(query)
     -- if it dont match, just search it
-else os.execute('xdg-open https://searx.be/search?q='..query_s)
+else openUrl('https://searx.be/search?q='..query_s)
 end
 
